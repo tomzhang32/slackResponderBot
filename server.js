e7635fdbf30e422fdd94cc14596e0d9378bd7029
Tom Zhang <tzhang@evernote.com>
2016-01-12 10:16:36 -0800

    Form a "callback maker" to extract error checking to a common function.

--- server.js
+++ server.js
@@ -1,25 +1,46 @@
-// server.js
+/*
+ * Slack responder bot. A simple bot to respond to your slack messages.
+ */
+
+'use strict'
+
 var request = require('request');
 
 var config = require('./config.json');
 
 var token = config.token;
 
+/**
+ * Function to make a callback that will do error-checking. If request is successful,
+ * checks the ok param in the response and calls successFun or errorFun appropriately
+ */
+var apiCallbackMaker = function(successFun, errorFun) {
+  return function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var data = JSON.parse(body);
+      if (data.ok) {
+        successFun(data);
+      } else {
+        errorFun(data);
+      }
+    }
+  }
+};
+
 var callApi = function(method, params, callback) {
   var targetUrl = 'https://slack.com/api/' + method;
   params = params || {};
   params.token = token;
+  callback = callback ||
+    apiCallbackMaker(console.log, function(data) {
+      console.log('Error encountered: ' + data.error)
+    });
 
   request({
     method: 'POST',
     url: targetUrl,
     form: params
-  }, function(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body);
-    }
-  });
+  }, callback);
 };
 
-callApi('rtm.start');
-
+var rtmStart = callApi('rtm.start', {});
